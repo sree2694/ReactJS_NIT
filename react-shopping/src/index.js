eef69e40b980f89adfde9b801d5cebeb15b3f316
sreerr <58017164+sree2694@users.noreply.github.com>
2023-03-20 12:37:57 +0530

    Update

--- react-shopping/src/index.js
+++ react-shopping/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Login } from './components/login';
 import './index.css';
-import { NetflixIndex } from './netflix/netflix-index';
-import{login} from './components/login'
+// import { NetflixIndex } from './netflix/netflix-index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
