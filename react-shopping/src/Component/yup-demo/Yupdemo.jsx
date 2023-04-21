a1579b51adad7dd3dbb4d2ef649bc20407d7e686
sreerr <58017164+sree2694@users.noreply.github.com>
2023-04-21 14:05:37 +0530

    update

---
+++ react-shopping/src/Component/yup-demo/Yupdemo.jsx
@@ -0,0 +1,59 @@
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+export function YupDemo(){
+
+    const formik = useFormik({
+        initialValues : {
+            "UserName":"",
+            "Age": 0,
+            "City":"",
+            "Mobile":""
+        },
+        onSubmit: (values)=>{
+            alert(JSON.stringify(values));
+        },
+        validationSchema: yup.object({
+            "UserName": yup.string()
+                           .required("User Name Required")
+                           .min(4, "Name too short..")
+                           .max(10, "Name too long.."),
+            "Age": yup.number()
+                      .required("Age Required"),
+            "Mobile": yup.string()
+                         .required("Mobile Required")
+                         .matches(/\+91\d{10}/, "Invalid Mobile +91 with 10 digits")
+        })
+    })
+
+    return(
+        <div className="container-fluid">
+        <form onSubmit={formik.handleSubmit}>
+        <h2>Register - Formik Yup</h2>
+         <dl>
+             <dt>User Name</dt>
+             <dd><input type="text" onChange={formik.handleChange}  name="UserName"/></dd>
+             <dd className="text-danger">{formik.errors.UserName}</dd>
+             <dt>Age</dt>
+             <dd><input type="text" onChange={formik.handleChange} name="Age" /></dd>
+             <dd className="text-danger">{formik.errors.Age}</dd>
+             <dt>City</dt>
+             <dd>
+                 <select onChange={formik.handleChange}  name="City">
+                     <option value="-1">Choose City</option>
+                     <option value="Delhi">Delhi</option>
+                     <option value="Hyd">Hyd</option>
+                 </select>
+             </dd>
+             <dd className="text-danger"></dd>
+             <dt>Mobile</dt>
+             <dd>
+                 <input type="text" onChange={formik.handleChange} name="Mobile" />
+             </dd>
+             <dd className="text-danger">{formik.errors.Mobile}</dd>
+         </dl>
+         <button>Register</button>
+        </form>
+     </div>
+    )
+}
